32566d2d2d1f110492e55384698645d3501b2263
Emine Kaplan <eminekaplantr@gmail.com>
2020-11-01 22:54:23 +0000

    adding file

---
+++ odd.js
@@ -0,0 +1,11 @@
+var num;
+funtion isOdd(num){
+	
+	if(num%2!===0){
+		console.log(num+" is odd number");
+	}else{
+		console.log(num+" is even number");
+	}
+
+}
+console.log("3 is odd: " + isOdd(3));
