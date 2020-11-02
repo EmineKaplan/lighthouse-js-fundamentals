d36f9ad419e6009294c984fe9973c50afb373f39
Emine Kaplan <eminekaplantr@gmail.com>
2020-11-02 01:22:37 +0000

    adding odd.js

--- odd.js
+++ odd.js
@@ -1,11 +1,4 @@
-var num;
-funtion isOdd(num){
-	
-	if(num%2!===0){
-		console.log(num+" is odd number");
-	}else{
-		console.log(num+" is even number");
-	}
-
+const isOdd = function (num) {
+  return num % 2 !== 0;
 }
 console.log("3 is odd: " + isOdd(3));
